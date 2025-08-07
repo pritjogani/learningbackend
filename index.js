9db05b7f800881dca7ac7ce09b41ea9375dff786
pritjogani <pritjogani.ast@gmail.com>
2025-08-07 11:49:23 +0530

    add json data

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ const app = express();
 
 const port = process.env.PORT;
 
+const github = {
+    "userId": 1,
+  "id": 1,
+  "title": "delectus aut autem",
+  "completed": false
+}
 app.get('/' ,(req,res) =>{
     res.send("home page")
 }
@@ -14,6 +20,9 @@ app.get("/login",(req,res) =>{
     res.send("login page")
 })
 
+app.get("/githubdata",(req,res) =>{
+    res.json(github)
+})
 app.listen(port ,  () =>{
     console.log(`app lisen sucessfully ${port} `)
 })
